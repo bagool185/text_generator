3879386a77b641dc7fbeb4e5cac708aaa01f6919
bagool185 <ioan.adrian98@gmail.com>
2019-01-03 02:33:08 +0000

    added is_next_line function

--- generator.py
+++ generator.py
@@ -3,9 +3,21 @@ from Chain import Chain
 
 import sys
 
-def main():
 
-    if (len(sys.argv) > 1): # if the filename is passed as an argument
+def is_next_line(word_count: int) -> bool:
+    """
+    Check if a line has enough words
+    :param word_count: number of words in the line
+    :return: True if enough words, False otherwise
+    """
+
+    words_per_line = 10
+
+    return word_count % words_per_line == 10 and word_count > words_per_line - 1
+
+
+def main():
+    if len(sys.argv) > 1:  # if the filename is passed as an argument
         parser = Parser(sys.argv[1])
 
     else:
@@ -15,13 +27,12 @@ def main():
     states = parser.get_states()
     chain = Chain(states[0], states)
 
-    OFFSET = 10 # words per line
     # Generate the text.
     for index in enumerate(states):
         chain.get_next_state()
 
-        if index[0] % OFFSET == 0 and index[0] > OFFSET - 1:
-           print('')
+        if is_next_line(index[0]):
+            print('')
 
 
 if __name__ == "__main__":
